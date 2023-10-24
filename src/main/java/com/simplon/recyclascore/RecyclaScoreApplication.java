68a9984ead86b2dd7890f3281ef54fcbec8152d6
Deshayes dayann <dayanndeshayes@gmail.com>
2023-10-24 10:06:59 +0200

    inot projet and archi

---
+++ src/main/java/com/simplon/recyclascore/RecyclaScoreApplication.java
@@ -0,0 +1,13 @@
+package com.simplon.recyclascore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecyclaScoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecyclaScoreApplication.class, args);
+	}
+
+}
