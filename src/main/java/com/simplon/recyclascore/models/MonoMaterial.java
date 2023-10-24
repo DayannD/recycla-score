000f736aba87b360e8c311e123e996190efb5e1b
Deshayes dayann <dayanndeshayes@gmail.com>
2023-10-24 11:32:43 +0200

    create connection to bdd

---
+++ src/main/java/com/simplon/recyclascore/models/MonoMaterial.java
@@ -0,0 +1,26 @@
+package com.simplon.recyclascore.models;
+
+import com.simplon.recyclascore.models.EnumMatererial.EnumMaterial;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "monomaterial", schema = "recycla-score", catalog = "")
+public class MonoMaterial {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private EnumMaterial material;
+    private String recyclability;
+}
+
+
