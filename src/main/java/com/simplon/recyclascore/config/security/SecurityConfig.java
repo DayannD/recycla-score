9632c4166ee5f59bd91171339e332a03e604cb82
Deshayes dayann <dayanndeshayes@gmail.com>
2023-10-24 17:24:45 +0200

    fix bug CORS

--- src/main/java/com/simplon/recyclascore/config/security/SecurityConfig.java
+++ src/main/java/com/simplon/recyclascore/config/security/SecurityConfig.java
@@ -1,8 +1,21 @@
 package com.simplon.recyclascore.config.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SecurityConfig {
-
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")  // Permet CORS pour toutes les routes
+                        .allowedOrigins("http://localhost:4200")  // Permet l'origine spécifiée
+                        .allowedMethods("*");  // Permet toutes les méthodes HTTP
+            }
+        };
+    }
 }
